1d12dc12820342a3cc5ffc7cd1fc84973d9e4d72
Максим Вараксин <maks-02@yandex.ru>
2022-06-21 21:53:25 +0300

    add additional

---
+++ app3/Program.cs
@@ -0,0 +1,11 @@
+﻿// addition
+
+Console.WriteLine("Введите первое число: ");
+string param_a = Console.ReadLine();
+
+Console.WriteLine("Введите второе число: ");
+string param_b = Console.ReadLine();
+
+int result = int.Parse(param_a) + int.Parse(param_b);
+
+Console.WriteLine($"Результат сложения = {result}");

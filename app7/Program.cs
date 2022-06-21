03bfa50c178bb26596d4b38c962516de1f1882f0
Максим Вараксин <maks-02@yandex.ru>
2022-06-21 23:06:27 +0300

    search max weight

---
+++ app7/Program.cs
@@ -0,0 +1,17 @@
+﻿// max weidth
+
+int a = 1;
+int b = 8;
+int c = 3;
+int d = 3;
+int e = 6;
+
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.WriteLine($"Максимальный вес гири = {max}");
+

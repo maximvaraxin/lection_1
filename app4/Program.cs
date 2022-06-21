9e4cb0c0aa849767022dec7a0cdb5ff2ee18b120
Максим Вараксин <maks-02@yandex.ru>
2022-06-21 22:21:12 +0300

    add division

---
+++ app4/Program.cs
@@ -0,0 +1,25 @@
+﻿// division int
+
+Console.WriteLine("Введите первое число: ");
+string param_a = Console.ReadLine();
+
+Console.WriteLine("Введите второе число: ");
+string param_b = Console.ReadLine();
+
+int result = int.Parse(param_a) / int.Parse(param_b);
+
+Console.WriteLine($"Результат сложения = {result}");
+
+// division double
+
+Console.WriteLine("Введите первое число: ");
+string param_c = Console.ReadLine();
+double param_C = Convert.ToDouble(param_c);
+
+Console.WriteLine("Введите второе число: ");
+string param_d = Console.ReadLine();
+double param_D = Convert.ToDouble(param_d);
+
+double res = param_C / param_D;
+
+Console.WriteLine($"Результат сложения = {res}");

c5428b45372749438e870ed4d12707f2a5124997
Максим Вараксин <maks-02@yandex.ru>
2022-06-21 22:43:23 +0300

    add if / else

---
+++ app6/Program.cs
@@ -0,0 +1,14 @@
+﻿// if-else
+
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+string user = username.ToLower();
+
+if(user == "мария")
+{
+    Console.WriteLine("Привет Машуня!");
+}
+else
+{
+    Console.WriteLine($"Ты зачем пришел {user}");
+}

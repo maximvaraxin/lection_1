905acd9dde40ede2b0fd68568ba35ef0072b220d
Максим Вараксин <maks-02@yandex.ru>
2022-06-21 21:37:52 +0300

    add hello user

---
+++ app2/Program.cs
@@ -0,0 +1,10 @@
+﻿//hello user
+
+Console.WriteLine("Ваше имя: ");
+string username =  Console.ReadLine();
+
+Console.WriteLine("Ваша фамилия: ");
+string user_lastname = Console.ReadLine();
+
+Console.WriteLine($"Доброго дня, {username} {user_lastname}");
+Console.Write($"Доброго дня,\n{username} {user_lastname}");

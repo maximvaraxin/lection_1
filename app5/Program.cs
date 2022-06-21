63fe006ce7349cef903d0f4c39b7f44152524064
Максим Вараксин <maks-02@yandex.ru>
2022-06-21 22:30:00 +0300

    add random

---
+++ app5/Program.cs
@@ -0,0 +1,10 @@
+﻿// random
+
+int param_a = new Random().Next(1,10);
+Console.WriteLine($"Первое случайное число = {param_a}");
+
+int param_b = new Random().Next(1,10);
+Console.WriteLine($"Второе случайное число = {param_b}");
+
+int result = param_a + param_b;
+Console.WriteLine($"Сумма случайных чисел = {result}");

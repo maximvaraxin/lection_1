c4ded7b6cb8e8d01be2b02d20c83a2ed4856752a
Максим Вараксин <maks-02@yandex.ru>
2022-06-22 16:06:13 +0300

    add task dog

---
+++ app9/Program.cs
@@ -0,0 +1,28 @@
+﻿// dog and friend
+
+int distanse = 2000;
+int speed_friend_one =15;
+int speed_friend_two =20;
+int speed_dog = 30;
+int stop_distanse = 10;
+
+int count = 0;
+int friend_to = 2;
+
+while(distanse > stop_distanse)
+{
+    if(friend_to == 1)
+    {
+        int time = distanse / (speed_friend_one + speed_dog);
+        friend_to = 2;
+    }
+    else
+    {
+        int time = distanse / (speed_friend_two + speed_dog);
+        friend_to = 1;
+    }
+    Console.WriteLine(distanse);
+    distanse = distanse - (speed_friend_one + speed_friend_two);
+    count++;
+}
+Console.WriteLine($"Собака пробежала: {count} раз.");
